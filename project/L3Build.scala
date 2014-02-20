182f5bdc1b2091cb28754abd11063105e4b4cdee
chrisg <chris@loyal3.com>
2014-02-19 16:09:02 -0800

    Initial Project Directory Commit

---
+++ project/L3Build.scala
@@ -0,0 +1,34 @@
+import sbt._
+
+object L3Build extends Build {
+  lazy val all = Project(
+    id   = "all",
+    base = file(".")
+  )
+  .aggregate(config, core, webUi)
+
+  lazy val config = Project(
+    id   = "config",
+    base = file("config")
+  )
+  .dependsOn(testTools % "test->compile;")
+
+  lazy val core = Project(
+    id   = "core",
+    base = file("core")
+  )
+  .dependsOn(testTools % "test->compile;", config)
+
+  lazy val webUi = Project(
+    id   = "web-ui",
+    base = file("web-ui")
+  )
+
+  lazy val testTools = Project(
+    id   = "test-tools",
+    base = file("test-tools")
+  )
+}
+
+
+
