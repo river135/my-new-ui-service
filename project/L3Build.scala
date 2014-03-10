360adc33f173e55b22557b15300da560c785c8d1
suresh <suresh@loyal3.com>
2014-03-10 14:35:48 -0700

    CSOP-8917 added scct configuration to plugins.sbt and build.scala files

--- project/L3Build.scala
+++ project/L3Build.scala
@@ -3,9 +3,10 @@ import sbt._
 object L3Build extends Build {
   lazy val all = Project(
     id   = "all",
-    base = file(".")
+    base = file("."),
+    settings = Defaults.defaultSettings ++ Seq(ScctPlugin.mergeReportSettings: _*)
   )
-  .aggregate(config, core, webUi)
+  .aggregate(config, util, webUi, webService, testTools)
 
   lazy val config = Project(
     id   = "config",
@@ -13,28 +14,33 @@ object L3Build extends Build {
   )
   .dependsOn(testTools % "test->compile;")
 
-  lazy val core = Project(
-    id   = "core",
-    base = file("core")
+  lazy val util = Project(
+    id   = "util",
+    base = file("util"),
+    settings = scctSettings
   )
   .dependsOn(testTools % "test->compile;", config)
 
   lazy val webUi = Project(
     id   = "web-ui",
-    base = file("web-ui")
+    base = file("web-ui"),
+    settings = scctSettings
   )
-  .dependsOn("core")
+  .dependsOn("util")
 
   lazy val webService = Project(
     id   = "web-service",
-    base = file("web-service")
+    base = file("web-service"),
+    settings = scctSettings
   )
-  .dependsOn("core")
+  .dependsOn("util")
 
   lazy val testTools = Project(
     id   = "test-tools",
     base = file("test-tools")
   )
+
+  lazy val scctSettings = Defaults.defaultSettings ++ Seq(ScctPlugin.instrumentSettings: _*)
 }
 
 
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -3,3 +3,7 @@ resolvers += "sbt-idea-repo" at "http://mpeltonen.github.com/maven/"
 addSbtPlugin("com.github.mpeltonen" % "sbt-idea" % "1.6.0")
 
 addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "0.9.2")
+
+// SCCT
+addSbtPlugin("com.sqality.scct" % "sbt-scct" % "0.2.2")
+
