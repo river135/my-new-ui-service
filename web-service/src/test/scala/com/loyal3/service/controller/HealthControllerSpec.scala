9dba0d9ef14d8b85a1048a405822720f86ed052d
Mark Castillo <mark@loyal3.com>
2014-03-11 13:51:59 -0700

    CSOP-8917 Added unit test for web-service and web-hi controllers

---
+++ web-service/src/test/scala/com/loyal3/service/controller/HealthControllerSpec.scala
@@ -0,0 +1,21 @@
+package com.loyal3.service.controller
+
+import com.twitter.finatra.FinatraServer
+import com.twitter.finatra.test.FlatSpecHelper
+
+class HealthControllerSpec extends FlatSpecHelper {
+
+  val server = new FinatraServer
+  server.register(new HealthApp)
+
+  class HealthApp extends HealthController {
+
+  }
+
+  "GET sample data with no parameter" should "respond 404" in {
+    get("/ping")
+    response.code should equal (200)
+    response.body should equal ("pong")
+  }
+
+}
---
+++ web-service/src/test/scala/com/loyal3/service/controller/SampleControllerSpec.scala
@@ -0,0 +1,25 @@
+package com.loyal3.service.controller
+
+import com.twitter.finatra.test.FlatSpecHelper
+import com.twitter.finatra._
+
+class SampleControllerSpec extends FlatSpecHelper {
+  val server = new FinatraServer
+  server.register(new SampleApp)
+
+  class SampleApp extends SampleController {
+
+  }
+
+  "GET sample data with no parameter" should "respond 404" in {
+    get("/api/v1/sampledata/")
+    response.code should equal (404)
+  }
+
+  "GET sample data" should "response OK" in {
+    get("/api/v1/sampledata/test_data")
+    response.code should equal (200)
+    response.body should equal ("""{"param":"test_data","data1":"This is the first line of the Sample Data","data2":"Some more stuff down here"}""")
+  }
+
+}
