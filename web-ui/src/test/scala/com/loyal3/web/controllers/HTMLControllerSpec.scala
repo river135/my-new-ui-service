9ce439844002a0120cfc3f2ee7e8a771f099d92a
Mark Castillo <mark@loyal3.com>
2014-03-11 15:56:17 -0700

    CSOP-8917 Fix HTMLController test

--- web-ui/src/test/scala/com/loyal3/web/controllers/HTMLControllerSpec.scala
+++ web-ui/src/test/scala/com/loyal3/web/controllers/HTMLControllerSpec.scala
@@ -8,20 +8,24 @@ class HTMLControllerSpec extends FlatSpecHelper {
   val server = new FinatraServer
   server.register(new HTMLApp)
 
+  //Configuring assets location
+  System.setProperty("com.twitter.finatra.config.docRoot", "web-ui/src/main/resources")
+  System.setProperty("com.twitter.finatra.config.assetPath", "app/")
+
   class HTMLApp extends HTMLController {
 
   }
 
-//  "GET root document directory" should "respond OK" in {
-//    get("/")
-//    response.code should equal (200)
-//  }
-//
-//
-//  "GET view" should "respond OK" in {
-//    get("/view/shell.html")
-//    response.code should equal (200)
-//  }
+  "GET root document directory" should "respond OK" in {
+    get("/")
+    response.code should equal (200)
+  }
+
+
+  "GET view" should "respond OK" in {
+    get("/view/shell.html")
+    response.code should equal (200)
+  }
 
 
 }
